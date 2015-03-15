8bdc305cee338ac747f0f3a4e70eaf9dacb9f81b
Andrew Kane <acekane1@gmail.com>
2015-03-15 00:46:08 -0700

    Added raise_envs

--- lib/robustly.rb
+++ lib/robustly.rb
@@ -3,7 +3,7 @@ require "errbase"
 
 module Robustly
   class << self
-    attr_accessor :env, :tag, :report_exception_method
+    attr_accessor :env, :raise_envs, :tag, :report_exception_method
 
     def report_exception(e)
       report_exception_method.call(e)
@@ -19,13 +19,14 @@ module Robustly
   self.env = ENV["RACK_ENV"] || ENV["RAILS_ENV"] || "development"
   self.tag = true
   self.report_exception_method = DEFAULT_EXCEPTION_METHOD
+  self.raise_envs = %w(development test)
 
   module Methods
     def safely(options = {})
       yield
     rescue *Array(options[:only] || StandardError) => e
       raise e if Array(options[:except]).any? { |c| e.is_a?(c) }
-      raise e if %w(development test).include?(Robustly.env)
+      raise e if Robustly.raise_envs.include?(Robustly.env)
       sample = options[:sample] || options[:throttle]
       if sample ? rand < 1.0 / sample : true
         begin
