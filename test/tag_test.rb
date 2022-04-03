183e27aa44e765473559fa0dc028699b05165bcc
Andrew Kane <andrew@ankane.org>
2022-04-02 21:24:11 -0700

    Improved tests [skip ci]

--- test/tag_test.rb
+++ test/tag_test.rb
@@ -2,35 +2,40 @@ require_relative "test_helper"
 
 class TagTest < Minitest::Test
   def test_tagged
-    ex = nil
-    Safely.report_exception_method = -> (e) { ex = e }
-    safely { raise Safely::TestError, "Boom" }
-    assert_equal "[safely] Boom", ex.message
+    assert_message("[safely] Boom") do
+      safely { raise Safely::TestError, "Boom" }
+    end
   end
 
   def test_not_tagged
     Safely.tag = false
-    ex = nil
-    Safely.report_exception_method = -> (e) { ex = e }
-    safely { raise Safely::TestError, "Boom" }
-    assert_equal "Boom", ex.message
+    assert_message("Boom") do
+      safely { raise Safely::TestError, "Boom" }
+    end
+  ensure
+    Safely.tag = true
   end
 
   def test_local_tag
-    ex = nil
-    Safely.report_exception_method = -> (e) { ex = e }
-    safely(tag: "hi") { raise Safely::TestError, "Boom" }
-    assert_equal "[hi] Boom", ex.message
+    assert_message("[hi] Boom") do
+      safely(tag: "hi") { raise Safely::TestError, "Boom" }
+    end
   end
 
   def test_report_exception_tag
+    assert_message("[safely] Boom") do
+      begin
+        raise Safely::TestError, "Boom"
+      rescue => e
+        Safely.report_exception(e)
+      end
+    end
+  end
+
+  def assert_message(expected)
     ex = nil
     Safely.report_exception_method = -> (e) { ex = e }
-    begin
-      raise Safely::TestError, "Boom"
-    rescue => e
-      Safely.report_exception(e)
-    end
-    assert_equal "[safely] Boom", ex.message
+    yield
+    assert_equal expected, ex.message
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,6 @@ end
 class Minitest::Test
   def setup
     Safely.env = "production"
-    Safely.tag = true
     Safely.report_exception_method = Safely::DEFAULT_EXCEPTION_METHOD
   end
 
